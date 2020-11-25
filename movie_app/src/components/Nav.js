9d500eeb898363bf2eb518b2c3f17cd84fe33adc
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-24 23:25:34 -0800

    imported styled components and cleaned up routes in Nav

--- movie_app/src/components/Nav.js
+++ movie_app/src/components/Nav.js
@@ -1,32 +1,30 @@
 import React from 'react';
+
+//React Router
 import { Switch, Link, Route } from 'react-router-dom';
+
+//Components
 import Home from './Home';
 import Movies from './Movies';
 import TvShows from './TvShows';
 import Trending from './Trending';
 
+//Styled Component Import
+
 function Nav() {
     return (
         <header>
             <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/movies'>Movies</Link>
-                <Link to='/tvshows'>Tv Shows</Link>
-                <Link to='/trending'>Trending</Link>
+                <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>Home</Link>
+                <Link style={{ textDecoration: 'none', color: 'black' }} to='/movies'>Movies</Link>
+                <Link style={{ textDecoration: 'none', color: 'black' }} to='/tvshows'>Tv Shows</Link>
+                <Link style={{ textDecoration: 'none', color: 'black' }} to='/trending'>Trending</Link>
             </nav>
             <Switch>
-                <Route path='/trending'>
-                    <Trending />
-                </Route>
-                <Route path='/tvshows'>
-                    <TvShows />
-                </Route>
-                <Route path='/movies'>
-                    <Movies />
-                </Route>
-                <Route path='/'>
-                    <Home />
-                </Route>
+                <Route path='/trending' component={Trending} />
+                <Route path='/tvshows' component={TvShows} />
+                <Route path='/movies' component={Movies} />
+                <Route path='/' component={Home} />
             </Switch>
         </header>
     );
